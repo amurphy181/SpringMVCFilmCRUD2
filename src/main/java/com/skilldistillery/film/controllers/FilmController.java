030b365c8f9851d0040aa09e7ec51edd3827e158
Alex Murphy <alexmurphy@Andrews-MacBook-Pro-2.local>
2019-11-01 15:26:31 -0600

    made changes to the film controller

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,8 +1,14 @@
 package com.skilldistillery.film.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FilmController {
+	
+	@RequestMapping(path = "goHome.do")
+	public String goHome() {
+		return "info.jsp";
+	}
 
 }
